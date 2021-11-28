8c3d457378cfa35da2cbcc5bf8c3e4e6b405760f
azu0nyan <nikgavgav@gmail.com>
2021-11-28 15:57:34 +0300

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "scalaLzw"
+
+version := "0.1"
+
+scalaVersion := "2.13.7"
---
+++ src/main/scala/Decoder.scala
@@ -0,0 +1,10 @@
+import scala.collection.mutable
+
+object Decoder {
+  def apply(in: Array[Byte]):Array[Byte] = {
+    val dict: mutable.Map[Seq[Byte], String] = Helpers.initialDict
+
+
+    in
+  }
+}
